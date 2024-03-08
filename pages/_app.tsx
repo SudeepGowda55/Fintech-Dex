8ef2c0c83a038d6f0995a810c9a84bc19203cc52
SudeepGowda55 <ssudeepgowda55@gmail.com>
2024-03-08 23:49:14 +0530

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,43 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { WagmiConfig, createConfig } from "wagmi";
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultConfig,
+} from "connectkit";
+import { useEffect, useState } from "react";
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: "rrVgolM4nvMB2LKUvN3D5TDOHFUvt82E",
+    walletConnectProjectId: "d34a101b0137dba5a615d1c6b5eb6f6b",
+
+    // Required
+    appName: "Fintech-Agent",
+
+    // Optional
+    appDescription: "Fintech Agent",
+  })
+);
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
+  return (
+    <div
+      style={{
+        padding: "20px",
+      }}
+    >
+      <WagmiConfig config={config}>
+        <ConnectKitProvider>
+          <ConnectKitButton />
+          {mounted && <Component {...pageProps} />}
+        </ConnectKitProvider>
+      </WagmiConfig>
+    </div>
+  );
+}
